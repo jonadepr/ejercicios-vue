6c1e67ffe1e43693a3d913719a25078a9f8e65b3
jonadepr <jonadepr@gmail.com>
2020-07-08 13:04:37 +0200

    vue components

---
+++ 03-cli/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{pruebas}}</h1>
+    <button @click="incrementaContador">Hola</button>
+    <h1>{{contador}}</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App" msg2="hola mundo"
+    v-bind:contador="contador" :obj="pruebaObj"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      pruebas: "PRUEBAS DATA NUMBER ONE",
+      contador: 0,
+      pruebaObj:{
+        texto: "hola miundo"
+      }
+      
+      }
+  },
+  methods: {
+    incrementaContador(){
+      this.contador++;
+    }
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 03-cli/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{msg2}}</h1>
+
+    <button @click="cambiaDatos">Cambiar desde Hijo</button>
+    <h1>{{obj.texto}}</h1>
+    <h1>{{contador}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+    msg2: {
+      type: String,
+      default: "Mensaje por defecto"
+    },
+    contador: {
+      type: Number,
+      default: 0
+    },
+    obj:{
+        texto: String
+      }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    cambiaDatos() {
+      this.contador++;
+      this.obj.texto = "perico";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
